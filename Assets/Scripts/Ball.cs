6260a5658398d7c983bd7bc5eb0e5b50eb461dd6
Sercan Altundas <srcnalt@users.noreply.github.com>
2023-02-05 16:44:15 +0200

    scene setup

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,9 +1,47 @@
+using System.Threading.Tasks;
+using OpenAI;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
+    [SerializeField] private Transform fog;
+    [SerializeField] private Material screen;
+    
+    private OpenAIApi openai = new OpenAIApi();
+
     private void Update()
     {
-        transform.Rotate(Vector3.forward, Time.deltaTime * 30);
+        fog.Rotate(Vector3.forward, Time.deltaTime * 30);
+    }
+
+    public async void SetImage()
+    {
+        var response = await openai.CreateImage(new CreateImageRequest
+        {
+            Prompt = "forest",
+            Size = ImageSize.Size256
+        });
+        
+        if (response.Data != null)
+        {
+            using(var request = new UnityWebRequest(response.Data[0].Url))
+            {
+                request.downloadHandler = new DownloadHandlerBuffer();
+                request.SendWebRequest();
+
+                while (!request.isDone) await Task.Yield();
+
+                Texture2D texture = new Texture2D(2, 2);
+                texture.LoadImage(request.downloadHandler.data);
+                
+                screen.SetTexture("_BaseMap", texture);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No image was created from this prompt.");
+        }
     }
 }
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using System.Threading.Tasks;
+using UnityEngine.Events;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 10;
+    [SerializeField] private Camera camera;
+    [SerializeField] private Transform pointOne;
+    [SerializeField] private Transform pointTwo;
+    [SerializeField] private bool runOnStart;
+
+    public UnityEvent OnCompleted;
+
+    private void Start()
+    {
+        MoveCamera();
+    }
+
+    public async void MoveCamera()
+    {
+        float progress = 0;
+
+        while (progress <= 1)
+        {
+            camera.transform.position = Vector3.Lerp(pointOne.position, pointTwo.position, progress);
+            progress += speed * Time.deltaTime;
+            await Task.Yield();
+        }
+        
+        OnCompleted?.Invoke();
+    }
+}
